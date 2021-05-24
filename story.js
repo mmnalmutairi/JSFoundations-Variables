e0a2983640312f4d04e2b35739c94daa7012033f
Maha Almutairi <carsridder10@gmail.com>
2021-05-24 19:58:32 +0300

    your message here

---
+++ story.js
@@ -0,0 +1,6 @@
+let Name = "Maha ";
+let Age = 26;
+let hobby = "Horse Riding";
+
+console.log(`${Name}, in 33 years, you will be ${Age + 33} years old. By then you'll have decades of ${hobby} practice! `);
+
